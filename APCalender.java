77e9b66a8f39ac7ff2927dd424c5151f3c8e31da
mitchellduea <54951211+mitchellduea@users.noreply.github.com>
2020-05-15 16:42:58 -0700

    Add files via upload

---
+++ APCalender.java
@@ -0,0 +1,34 @@
+public class APCalender{
+
+
+
+public static int numberOfLeapYears(int year1, int years2){
+  int count = 0;
+
+
+  for ( int a = year1; a<= year 2; a++){
+
+
+    if (isLeapYear(a)){
+
+
+      count ++;
+
+
+    }
+
+
+    return count;
+  }
+
+}
+public static int dayOfWeek(int month, int day, int year){
+  int BegginningDay= firstDAy(year);
+
+
+  int returnDay= (StartDay)
+
+  
+  int return day;
+}
+}
---
+++ Delimeters.java
@@ -0,0 +1,60 @@
+public class Delimeters{
+
+
+  
+public ArrayList<String>Delimeters(String[])
+
+
+{
+  for (String s : t)
+
+
+  if{str.equals() && str.equals()}
+
+
+}
+
+
+
+
+return s;
+return String;
+
+
+
+public boolean isBalanced(ArrayList<String> Delimeters)
+
+
+{
+  int count= 0;
+
+
+  int otherCount= 0
+
+
+  if(str.equals(){
+
+
+    count++;
+
+
+  }
+  if (count= otherCount){
+
+
+    return true;
+
+
+  }
+
+  else{
+
+
+    return false;
+
+
+  }
+
+
+}
+}
